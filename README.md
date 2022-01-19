8071634100d0eed1d0a29bf87da90e0c58289e0d
Rory Eckel <16675082+roryeckel@users.noreply.github.com>
2022-01-18 23:42:51 -0600

    Add POST, DELETE, more stuff

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Link shortening webserver which records basic analytics such as IP address
 
 Built with flask and sqlalchemy
+
+1. Set environment variables for SQLALCHEMY_DATABASE_URI and LINKD_SECRET ("admin" password)
+2. Use a wsgi server on linkd.py
--- linkd.py
+++ linkd.py
@@ -8,12 +8,14 @@ from flask import Flask, redirect, abort, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.middleware.proxy_fix import ProxyFix
 from datetime import datetime
+from functools import wraps
 import os
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['LINKD_SECRET'] = os.getenv('LINKD_SECRET')
 db = SQLAlchemy(
     app,
     session_options={
@@ -22,13 +24,20 @@ db = SQLAlchemy(
 
 class Link(db.Model):
     """Shortened URL with path"""
+    __tablename__ = 'link'
     path = db.Column(db.String(256), primary_key=True)
     url = db.Column(db.String(2048), nullable=False)
-    access_log = db.relationship('AccessLog')
+
+    def __init__(self, path, url):
+        """Construct new link"""
+        self.path = path
+        self.url = url
 
 class AccessLog(db.Model):
     """Access logs for Link"""
-    path = db.Column(db.String(256), db.ForeignKey('link.path'), primary_key=True, nullable=False)
+    __tablename__ = 'access_log'
+    path = db.Column(db.String(256), db.ForeignKey('link.path', onupdate='CASCADE', ondelete='CASCADE'), primary_key=True, nullable=False)
+    link = db.relationship(Link, backref=db.backref('access_log', cascade='all, delete-orphan'), lazy='joined')
     ip = db.Column(db.String(45), primary_key=True, nullable=False)
     when = db.Column(db.DateTime(), primary_key=True, nullable=False, default=datetime.utcnow)
 
@@ -37,23 +46,61 @@ class AccessLog(db.Model):
         self.path = path
         self.ip = ip
 
+if not db:
+    raise Exception('Unable to open SQLALCHEMY_DATABASE_URI')
 db.create_all()
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def linkd(path):
-    """Catch all paths: index template or link redirect with logging"""
-    if not path:
-        return render_template(
-            'index.html',
-            links=Link.query.count(),
-            visits=AccessLog.query.count())
-    link = Link.query.filter_by(path = path).first()
+def secret_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.values.get('LINKD_SECRET') != app.config['LINKD_SECRET']:
+            abort(401)
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/', methods=['GET'])
+def index():
+    """Index template"""
+    if request.values.get('LINKD_SECRET') == app.config['LINKD_SECRET']:
+        return Link.query.all()
+    return render_template(
+        'index.html',
+        links=Link.query.count(),
+        visits=AccessLog.query.count())
+
+@app.route('/<path:path>', methods=['POST'])
+@secret_required
+def linkd_post(path):
+    url = request.form.get('url')
+    if not url:
+        abort(400)
+    link = Link.query.filter_by(path=path).first()
     if link:
-        db.session.add(AccessLog(path, request.remote_addr))
-        db.session.flush()
-        return redirect(link.url)
-    abort(404)
+        link.url = url
+    else:
+        db.session.add(Link(path, url))
+    db.session.flush()
+    return '', 204
+
+@app.route('/<path:path>', methods=['GET'])
+def linkd_get(path):
+    """Catch all subpaths: link redirect with logging"""
+    link = Link.query.filter_by(path=path).first()
+    if not link:
+        abort(404)
+    db.session.add(AccessLog(path, request.remote_addr))
+    db.session.flush()
+    return redirect(link.url)
+
+@app.route('/<path:path>', methods=['DELETE'])
+@secret_required
+def linkd_delete(path):
+    link = Link.query.filter_by(path=path).first()
+    if not link:
+        abort(404)
+    db.session.delete(link)
+    db.session.flush()
+    return '', 204
 
 if __name__ == "__main__":
     app.run()
